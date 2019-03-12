1c53b29cad805b1a9a72767c175691abf3f91fe1
Brian McClain <brianmmcclain@gmail.com>
2019-03-12 10:14:46 -0400

    Initial commit, bundled static content

---
+++ src/main/java/com/github/brianmmcclain/cfroutingdemo/CfRoutingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.brianmmcclain.cfroutingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfRoutingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfRoutingDemoApplication.class, args);
+	}
+
+}
