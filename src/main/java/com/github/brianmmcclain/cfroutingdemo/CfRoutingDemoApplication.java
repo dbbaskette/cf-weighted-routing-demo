b4551618b869ee281e9e342b41f532e4300f6ab8
Brian McClain <brianmmcclain@gmail.com>
2019-03-31 18:49:40 -0400

    Add HTTP Basic auth to actuator endpoints

--- src/main/java/com/github/brianmmcclain/cfroutingdemo/CfRoutingDemoApplication.java
+++ src/main/java/com/github/brianmmcclain/cfroutingdemo/CfRoutingDemoApplication.java
@@ -2,12 +2,25 @@ package com.github.brianmmcclain.cfroutingdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class CfRoutingDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfRoutingDemoApplication.class, args);
+	}
+
+	@Configuration
+	public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+		public void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+				.antMatchers("/actuator/**").hasRole("ACTUATOR")
+				.anyRequest().permitAll()
+			.and().httpBasic();
+		}
 	}
 
 }
